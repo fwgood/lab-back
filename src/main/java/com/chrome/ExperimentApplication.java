ba1e534d7a4041b2fc1884eef4ba454bf1e8330f
a957815154 <957815154@qq.com>
2018-09-14 09:36:11 +0800

    commit war

--- src/main/java/com/chrome/ExperimentApplication.java
+++ src/main/java/com/chrome/ExperimentApplication.java
@@ -3,6 +3,8 @@ package com.chrome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Created with IDEA
@@ -11,16 +13,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Time:9:48
  */
 //测试环境
-@SpringBootApplication
+/*@SpringBootApplication
 public class ExperimentApplication {
 
 
     public static void main(String[] args) {
         SpringApplication.run(ExperimentApplication.class, args);
     }
-}
+}*/
 //开发环境配置
-/*
 @SpringBootApplication
 public class ExperimentApplication extends SpringBootServletInitializer {
 
@@ -33,5 +34,4 @@ public class ExperimentApplication extends SpringBootServletInitializer {
         SpringApplication.run(ExperimentApplication.class, args);
     }
 }
-*/
 
